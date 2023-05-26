463b7db9b4d6604605cf8711802b072d1882b8a8
Kanisse <73669714+Kanisse@users.noreply.github.com>
2023-05-26 15:10:18 +0100

    connexion

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
+<?php include "connexion.php";?>
 <html>
-    
 
 <head>
     <title> Page de connexion</title>
@@ -18,7 +18,7 @@
             <td><input type="password" name="password"></td>
         </tr>
         <tr>
-            <td> <input type="submit" value="Se connecter"> </td>
+            <td> <input type="submit" value="Se connecter" name="OK"> </td>
             <td>  <input type="reset" value="Reset"> </td>
         </tr>
     </table>
@@ -26,3 +26,28 @@
 </form>
 </body>
 </html>
+
+
+<?php  
+
+if(isset($_POST['OK'])){
+$login = $_POST['login'];
+$password= $_POST['password'];
+
+$sql = "select * from utilisateurs";
+$result = $connexion -> query ($sql);
+
+while ($ligne = $result -> fetch_array()){
+    if ($ligne['login'] == $login && $ligne['mdp'] == md5($password)){
+header('Location: acceuil.php');
+    }
+    else {
+        echo " <script> alert(' login ou mot de passe incorrect ') </script> ";
+    }
+}
+}
+
+
+
+
+?>
